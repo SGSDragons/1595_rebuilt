e3635bf152a2274f45f28742c78c7ecf17c00180
donovanbradford <donovan.c.bradford@gmail.com>
2026-02-15 14:58:33 -0800

    Remove examples. Add our drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,13 +5,12 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Axis;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -25,36 +24,55 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final SwerveSubsystem swerve = new SwerveSubsystem(Units.MetersPerSecond.of(8), Pose2d.kZero);
+  private final SwerveSubsystem drive = new SwerveSubsystem(Units.MetersPerSecond.of(8), Pose2d.kZero);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController driverController = new CommandXboxController(OperatorConstants.driverControllerPort);
+  private final CommandXboxController operatorController = new CommandXboxController(OperatorConstants.operatorControllerPort);
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
+    if (drive instanceof SwerveSubsystem) {
+      SwerveSubsystem swerve = (SwerveSubsystem) drive;
+      swerve.resetOdometry(Pose2d.kZero);
+    }
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+  //      ______________________________(17, 8)
+  //      |                                |
+  //     B|                                |R
+  //     B|                                |R
+  //     B|                                |R
+  //     B|                                |R
+  //      |________________________________|
+  //    (0,0)
+  //
+  // With 0deg heading pointing right.
+  //
+  // When on the Blue alliance
+  //    Left joystick Up (-1 y-axis) maps to positive X.
+  //    Left joystick left (-1 x-axis) maps to positive Y.
+  // Thus, Blue axis are inverted.
+  class DriverSticks {
+    // TODO: Make this adapt to the Red/Blue alliance
+    private final double inverter = 1.0;
+    double readAxis(XboxController.Axis axis) {
+      return driverController.getRawAxis(axis.value);
+    }
+    public double translateX() { return inverter*readAxis(Axis.kLeftY); }
+    public double translateY() { return inverter*readAxis(Axis.kLeftX); }
+    public double lookX() { return inverter*readAxis(Axis.kRightX); }
+    public double lookY() { return inverter*readAxis(Axis.kRightY); }
+  }
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  private void configureBindings() {
+
+    DriverSticks driver = new DriverSticks();
+    
+    drive.setDefaultCommand(drive.driveCommand(driver::translateX, driver::translateY, driver::lookX, driver::lookY, 1.0));
   }
 
   /**
@@ -64,6 +82,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
